b41694341908ca35458a15f49ed23cddc0e51a09
Gene Conroy-Jones <gene@projectdado.com>
2022-12-06 10:46:27 -0800

    Part two

--- 02/day2.py
+++ 02/day2.py
@@ -18,6 +18,12 @@ WINNER_DICT = {
     "C": "B"    # Scissors beats Paper
 }
 
+LOSER_DICT = {
+    "A": "B",   # Rock loses to Paper
+    "B": "C",   # Paper loses to Scissors
+    "C": "A"    # Scissors loses to Rock
+}
+
 SELECTED_SCORE = {
     "A": 1,
     "B": 2,
@@ -25,7 +31,7 @@ SELECTED_SCORE = {
 }
 
 
-def fetch_and_decrypt_input():
+def fetch_and_decrypt_input(col2_lambda):
     results = []
     # open the file and read its contents
     with open(INPUT_FILE) as file:
@@ -34,9 +40,24 @@ def fetch_and_decrypt_input():
             # append the line to the list, stripping any whitespace characters
             # from the beginning and end of the line
             row = line.rstrip('\n').split(' ')
-            row[1] = DECRYPT_RESPONSE[row[1]]
+            row[1] = col2_lambda(row[0], row[1])
             results.append(row)
     return results
+
+
+def part_two_decrypt(opponent, outcome):
+    """ Outcome
+        X = Lose
+        Y = Draw
+        Z = Win
+    """
+
+    if outcome == 'Y':
+        return opponent
+    elif outcome == 'X':
+        return WINNER_DICT[opponent]
+    elif outcome == 'Z':
+        return LOSER_DICT[opponent]
 
 
 def calculate_score(input):
@@ -49,14 +70,22 @@ def calculate_score(input):
         elif val[0] == WINNER_DICT[val[1]]:
             win_score = 6
         round_score = selected_shape_score + win_score
-        print(round_score)
         total += round_score
     return total
 
 
 if __name__ == "__main__":
-    print('Day 2')
-    data = fetch_and_decrypt_input()
-    print(data)
+    # Part 1
+    print('Day 2 - Part 1')
+    col2_lambda = lambda col0, col1: DECRYPT_RESPONSE[col1]
+    data = fetch_and_decrypt_input(col2_lambda)
     total_score = calculate_score(data)
     print(total_score)
+
+    # Part 2
+    print('Day 2 - Part 2')
+    col2_lambda = lambda col0, col1: part_two_decrypt(col0, col1)
+    data = fetch_and_decrypt_input(col2_lambda)
+    total_score = calculate_score(data)
+    print(total_score)
+
