a70a3bad91cea32ab539b9a6b91a4a01bb3cc2f9
Gene Conroy-Jones <gene@projectdado.com>
2022-12-14 13:00:39 -0800

    Part 1

--- 13/day13.py
+++ 13/day13.py
@@ -1,7 +1,7 @@
 import heapq as heap
 from collections import defaultdict
 import json
-
+from functools import cmp_to_key
 INPUT_FILE = '13/input.txt'
 
 # Distress Signal
@@ -28,13 +28,16 @@ def ensure_array(val):
     else:
         return [val]
 
+def is_array(val):
+    return type(val) == list
+
 def extract_array_val(val, idx):
     return None if idx >= len(val) else val[idx]
 
 def either_is_array(lval, rval):
     return type(lval) == list or type(rval) == list
 
-DEBUG = False
+DEBUG = True
 def debug_print(msg):
     if DEBUG:
         print(msg)
@@ -45,9 +48,11 @@ def is_left_smaller(lval, rval):
     result = None
     finished = False
     idx = 0
+    debug_print(f'Analyzing: run_test({json.dumps(lval)}, {json.dumps(rval)})')
     while not finished:
         curr_lval = extract_array_val(lval, idx)
         curr_rval = extract_array_val(rval, idx)
+        print(curr_lval, curr_rval)
 
         if result is False:
             debug_print('ended')
@@ -55,7 +60,9 @@ def is_left_smaller(lval, rval):
         elif curr_lval is None or curr_rval is None:
             debug_print('either val is None')
             # End as we've ended lists
-            if result is None:
+            if is_array(curr_lval) and curr_rval is None:
+                result = False
+            elif result is None:
                 debug_print('result is None')
                 result = False if curr_rval is None and curr_lval is not None else True
             elif curr_lval is not None and curr_rval is None:
@@ -80,7 +87,7 @@ def is_left_smaller(lval, rval):
             result = True
             finished = True
         idx += 1
-    print(f'Analyzing: run_test({json.dumps(lval)}, {json.dumps(rval)}) -> {result}')
+    debug_print(f'Result: run_test({json.dumps(lval)}, {json.dumps(rval)}) -> {result}')
     return result
 
 
@@ -90,10 +97,9 @@ def run_test(lval, rval, expected):
     print(f'test_success: {test_success} value expected {expected}\n\n')
 
 def test_cases():
-    run_test([[2], 9], [3], True)
-    run_test([2, 3, 4], [4], True)
-    run_test([], [8], True)
-    run_test([[], 2, 9, 4], [[8], [3, 6, 9, [10, 4, 2], [4, 5]], [10, [3], [7, 10, 7, 2], [0, 5]], 2], True)
+    run_test([[[]]], [[]], False)
+    run_test([[[3]]], [[3]], False)
+
 
 def test_cases_all():
     run_test([1,1,3,1,1], [1,1,5,1,1], True)
@@ -106,6 +112,8 @@ def test_cases_all():
     run_test([[[]]], [[]], False)
     run_test([1,[2,[3,[4,[5,6,7]]]],8,9], [1,[2,[3,[4,[5,6,0]]]],8,9], False)
     run_test([[2], 9], [3], True)
+    run_test([[[]]], [[]], False)
+    run_test([[[2]]], [[2]], False)
 
 
 def part_one():
@@ -126,10 +134,54 @@ def part_one():
     print(correct_order)
     print(sum(correct_order))
 
+def bubbleSort(arr):
+    n = len(arr)
+    # optimize code, so if the array is already sorted, it doesn't need
+    # to go through the entire process
+    swapped = False
+    # Traverse through all array elements
+    for i in range(n-1):
+        # range(n) also work but outer loop will
+        # repeat one time more than needed.
+        # Last i elements are already in place
+        for j in range(0, n-i-1):
 
+            # traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            left_smaller = is_left_smaller(arr[j], arr[j + 1])
+            if not left_smaller:
+                swapped = True
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
+        if not swapped:
+            # if we haven't needed to make a single swap, we
+            # can just exit the main loop.
+            return
 
+def part_two():
+    raw_results = fetch_input()
+    raw_results.append([[2]])
+    raw_results.append([[6]])
+
+    print('Sorted')
+    bubbleSort(raw_results)
+    lower = -1
+    upper = -1
+    idx = 1
+    for r in raw_results:
+        print (idx)
+        if r == [[2]]:
+            print("Found Lower")
+            lower = idx
+        if r == [[6]]:
+            print("Found Upper")
+            upper = idx
+        idx += 1
+        print(r)
+    print((lower, upper))
+    print(lower * upper)
 
 if __name__ == "__main__":
-    part_one()
+    test_cases()
 
